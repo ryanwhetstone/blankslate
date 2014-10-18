573477c00e2e1bd9c4475ca34eb006be0bff9af1
ryanwhetstone <whetstone.ryan@gmail.com>
2014-10-18 13:39:57 -0400

    Updated functions.php

---
+++ lib/custom/actions.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Example: Register widget
+ *
+ * function _usdp_register_widget() {
+ *   register_sidebar(array(
+ *     // ...
+ *   ));
+ * }
+ * add_action('widgets_init', '_usdp_register_widget');
+ */
---
+++ lib/custom/helpers.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Example: Get child pages as array
+ *
+ * function _usdp_get_page_children($parent_id) {
+ *   $page_children = get_pages(array(
+ *     'child_of' => $parent_id,
+ *     'sort_column' => 'menu_order'
+ *   ));
+ *
+ *   return $page_children;
+ * }
+ */
